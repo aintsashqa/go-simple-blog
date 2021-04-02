fdcc811fef7eb601800683f5f618798aa9970e5d
Alex <ujeromi@gmail.com>
2021-04-02 14:42:49 +0300

    Removed v1 response consts

--- internal/delivery/http/v1/response.go
+++ internal/delivery/http/v1/response.go
@@ -7,13 +7,6 @@ import (
 	"github.com/go-chi/render"
 )
 
-const (
-	internalErrorMsg             string = "Internal error"
-	validationFailedMsg          string = "Validation failed"
-	invalidUrlQueryParamErrorMsg string = "Invalid url query parameter `%s`"
-	authorizationFailedMsg       string = "Authorization failed"
-)
-
 func respond(w http.ResponseWriter, r *http.Request, status int, payload interface{}) {
 	render.Status(r, status)
 	render.JSON(w, r, payload)
